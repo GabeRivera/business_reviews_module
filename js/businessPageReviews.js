7b597bbba89d93ef9a97b09b432729390bc8ecac
Gabe Rivera <gabriel.rivera@corp.ovh.us>
2019-02-22 11:31:55 -0500

    simpliefied drupal behaviors code

--- js/businessPageReviews.js
+++ js/businessPageReviews.js
@@ -1,38 +1,22 @@
-import {html, render} from 'lit-html';
+import {
+  html,
+  render
+} from 'lit-html';
 import './components/PaginationContainer.js';
 
 (function reviewsList($, Drupal) {
   Drupal.behaviors.reviewsList = {
     attach(context, settings) {
       $(context).find('#reviews-list').once('reviewsList').each(() => {
-
         const reviews = [...settings.reviews, ...settings.reviews, ...settings.reviews, ...settings.reviews];
-
-        const cardItem = (item) => html`
-            <div class="col-md-6">
-                <div class="card t10-card">
-                  <div class="card-item card-title">
-                      <h3>${item.title}</h3>
-                      <span class="subtitle">
-                      by ${item.firstName} ${item.lastName} | ${item.dateSubmitted}
-                      </span>
-                  </div>
-                  <div class="card-item card-text">
-                      <p>${item.content}</p>
-                  </div>
-                </div>
-            </div>
-            `;
-          const stringifiedReviews = JSON.stringify(reviews);
-          const cardList = (list) => html`
+        const stringifiedReviews = JSON.stringify(reviews);
+        const cardList = (list) => html `
             <pagination-container
               currPage="0"
               pages=${stringifiedReviews}
             ></pagination-container>
-            ${list.map(item => html`${cardItem(item)}`)}
           `;
-
-          render(cardList(reviews), document.getElementById('reviews-list'));
+        render(cardList(reviews), document.getElementById('reviews-list'));
       });
     },
   };
